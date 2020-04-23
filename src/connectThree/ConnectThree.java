f0d4922b733d7f77e66c9db30ce4f1c02c690ee7
amelia <missblue747@gmail.com>
2020-04-23 11:52:06 -0500

    finished gameboard

--- src/connectThree/ConnectThree.java
+++ src/connectThree/ConnectThree.java
@@ -4,9 +4,13 @@ import comp127graphics.CanvasWindow;
 
 public class ConnectThree {
     CanvasWindow canvas;
+    gameBoard board;
 
     public ConnectThree(){
-        canvas = new CanvasWindow("Connect Three", 200, 200);
+        canvas = new CanvasWindow("Connect Three", 550, 500);
+        board = new gameBoard(50, 50);
+        board.addToCanvas(canvas);
+        canvas.draw();
     }
 
     public static void main(String[] args){
--- src/connectThree/gameBoard.java
+++ src/connectThree/gameBoard.java
@@ -1,4 +1,33 @@
 package connectThree;
 
+import comp127graphics.CanvasWindow;
+import comp127graphics.Ellipse;
+import comp127graphics.GraphicsGroup;
+import comp127graphics.Rectangle;
+
+import java.awt.*;
+
 public class gameBoard {
+    Rectangle rectangle;
+    GraphicsGroup board;
+    Ellipse space;
+    private final int CUSHION = 10, SPACE = 75, BOARD_WIDTH = 435, BOARD_HEIGHT = 350;
+
+    public gameBoard(int positionX, int positionY){
+        board = new GraphicsGroup();
+        rectangle = new Rectangle(positionX, positionY, BOARD_WIDTH, BOARD_HEIGHT);
+        rectangle.setFillColor(Color.yellow);
+        board.add(rectangle);
+        for (int x=positionX+CUSHION; x<=BOARD_WIDTH; x+=SPACE+CUSHION){
+            for(int y=positionY+CUSHION; y<=BOARD_HEIGHT; y+=SPACE+CUSHION) {
+                space = new Ellipse(x, y, SPACE, SPACE);
+                space.setFillColor(Color.white);
+                board.add(space);
+            }
+        }
+    }
+
+    public void addToCanvas(CanvasWindow canvas){
+        canvas.add(board);
+    }
 }
