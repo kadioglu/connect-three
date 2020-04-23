ecf5f113316024cfbedfd0557a5ff0c32b3dc183
amelia <missblue747@gmail.com>
2020-04-23 11:06:06 -0500

    gameboard

--- src/connectThree/connectThree.java
+++ src/connectThree/connectThree.java
@@ -1,5 +1,12 @@
 package connectThree;
 
+import comp127graphics.CanvasWindow;
+
 public class connectThree {
+    CanvasWindow canvas = new CanvasWindow("Connect Three", 200, 200);
+
+    public static void main(String[] args){
+
+    }
 
 }
